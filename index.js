ee7ec39f0439ca4bdabf982ea9dc793e9177b3c5
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-18 21:33:20 -0600

    added async function to get data from api

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello, puppers");
+const COHORT = "2309-AM";
+const API = "https://fsa-puppy-bowl.herokuapp.com/api/" + COHORT;
+
+const state = {
+  players: [],
+  selectedPlayer: null,
+};
+
+const getPlayers = async () => {
+  try {
+    const response = await fetch(API + "/players");
+    const json = await response.json();
+    state.players = json.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
