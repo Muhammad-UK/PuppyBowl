065f3417e569b4316b4fb53839d09abe98e592c9
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-18 22:17:31 -0600

    added renderplayers and init functions

--- index.js
+++ index.js
@@ -10,8 +10,34 @@ const getPlayers = async () => {
   try {
     const response = await fetch(API + "/players");
     const json = await response.json();
-    state.players = json.data;
+    state.players = json.data.players;
   } catch (error) {
     console.log(error);
   }
 };
+
+const renderPlayers = async () => {
+  await getPlayers();
+
+  const $players = document.querySelector("div.players");
+
+  $players.innerHTML = state.players
+    .map(
+      (player) => `
+        <section>
+            <div>
+                <h2>${player.name}</h2>
+                <p>${player.breed}</p>
+                <img src="${player.imageUrl}">
+            </div>
+        </section>
+    `
+    )
+    .join("");
+};
+
+const init = async () => {
+  renderPlayers();
+};
+
+init();
