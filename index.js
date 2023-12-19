1feccf9036695e74b3d3aa037d4e69b1c83e45e2
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-19 10:03:07 -0600

    added comments to js code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Defining the API and necessary information as per the API's documentation
 const COHORT = "2309-AM";
 const API = "https://fsa-puppy-bowl.herokuapp.com/api/" + COHORT;
 
+// Initializing a 'state' variable to control / hold all our current data
 const state = {
   players: [],
   selectedPlayer: null,
 };
 
+// Defining an async function to fetch data from the API for whatever specific data we want to use, in this case, data.players
 const getPlayers = async () => {
   try {
     const response = await fetch(API + "/players");
@@ -16,11 +19,13 @@ const getPlayers = async () => {
   }
 };
 
+// Defining another async function to render the data to the DOM, specifically just when there isn't any selected player
 const renderPlayers = async () => {
   await getPlayers();
 
   const $players = document.querySelector("div.players");
 
+  // Creating a 'section' element here so I can modify it later, probably could do this with the button as well but I used a different method there
   const $playersSection = state.players.map((player) => {
     const section = document.createElement("section");
     section.innerHTML = `
@@ -30,6 +35,7 @@ const renderPlayers = async () => {
         </div>
     `;
 
+    // Adding an event listener to add data to the selected player in state
     section.addEventListener("click", () => {
       state.selectedPlayer = player;
       render();
@@ -40,12 +46,12 @@ const renderPlayers = async () => {
   $players.replaceChildren(...$playersSection);
 };
 
+// Creating another function to render but only just one datapoint whenever it is !null
 const renderSinglePlayer = () => {
   const $players = document.querySelector("div.players");
   const $button = document.querySelector("div.button");
 
   $button.innerHTML = "<h2>Back to All Puppies</h2>";
-
   $players.innerHTML = `
         <section>
             <div>
@@ -56,6 +62,7 @@ const renderSinglePlayer = () => {
         </section>
     `;
 
+  // Checking if the 'button' (really it's just an h2) is clicked meaning the user wants to go back to the main page, then updating the state as necessary
   $button.querySelector("h2").addEventListener("click", () => {
     state.selectedPlayer = null;
     $button.replaceChildren();
@@ -64,6 +71,7 @@ const renderSinglePlayer = () => {
   });
 };
 
+// Defining a specific render function to reuse renderPlayers and renderSinglePlayer functions, checking the value of state first
 const render = () => {
   if (!state.selectedPlayer) {
     renderPlayers();
@@ -72,6 +80,7 @@ const render = () => {
   }
 };
 
+// Defining an async function to check the hash state as well as updating the url with the appropriate playerID as needed
 const loadPlayerFromHash = async () => {
   if (state.players.length === 0) {
     await getPlayers();
@@ -83,8 +92,10 @@ const loadPlayerFromHash = async () => {
   render();
 };
 
+// Using the loadPlayerFromHash in the next function means that I need to also use the function whenever the hash changes for the window
 window.addEventListener("hashchange", loadPlayerFromHash);
 
+// The initializer function that calls every other function as needed as they are all nested
 const init = async () => {
   loadPlayerFromHash();
 };
